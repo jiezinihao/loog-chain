47162a10988dd590bce1d25836a512d44788eb18
杨启柱 <1275401327@qq.com>
2026-08-08 17:56:55 +0800

    feat: 实现思想切面笔记瀑布流

--- packages/thinking/src/index.ts
+++ packages/thinking/src/index.ts
@@ -1,2 +1,5 @@
 /** 思想模块的稳定标识，供唯一入口后续注册。 */
 export const thinkingModule = 'thinking';
+
+export { default as ThinkingView } from './ThinkingView.vue';
+export { thinkingNotes, type ThinkingNote, type NoteWeight } from './notes';
